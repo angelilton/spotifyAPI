821381e2330f67f52a9a0055603a1e745a332ea4
mac <mac@macs-MacBook-Pro.local>
2020-06-22 14:30:25 +0100

    promise error

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-export const search = () => {};
-export const searchAlbums = () => {};
-export const searchTracks = () => {};
-export const searchArtists = () => {};
-export const searchPlaylists = () => {};
+export const search = (query, type) =>
+  fetch(
+    `https://api.spotify.com/v1/search?q=${query}&type=${type}`
+  ).then(data => data.json())
+export const searchAlbums = () => {}
+export const searchTracks = () => {}
+export const searchArtists = () => {}
+export const searchPlaylists = () => {}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,32 +1,85 @@
-import { expect } from "chai";
+import chai, { expect } from 'chai'
+import Sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
 import {
   search,
   searchAlbums,
   searchArtists,
   searchTracks,
-  searchPlaylists,
-} from "../src/main";
+  searchPlaylists
+} from '../src/main'
 
-describe("Spotify API", () => {
-  describe("smoke test", () => {
-    it("should exist the search method", () => {
-      expect(search).to.exist;
-    });
+global.fetch = require('node-fetch')
+chai.use(sinonChai)
 
-    it("should exist the searchAlbums method", () => {
-      expect(searchAlbums).to.exist;
-    });
+describe('Spotify API', () => {
+  describe('smoke test', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist
+    })
 
-    it("should exist the searchArtists method", () => {
-      expect(searchArtists).to.exist;
-    });
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist
+    })
 
-    it("should exist the searchTracks method", () => {
-      expect(searchTracks).to.exist;
-    });
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist
+    })
 
-    it("should exist the searchPlaylists method", () => {
-      expect(searchPlaylists).to.exist;
-    });
-  });
-});
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist
+    })
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist
+    })
+  })
+
+  describe('Generic Search', () => {
+    let stubedFetch
+    let promise
+
+    beforeEach(() => {
+      stubedFetch = Sinon.stub(global, 'fetch')
+      promise = stubedFetch.resolves({ json: () => ({ body: 'json' }) })
+    })
+    afterEach(() => {
+      stubedFetch.restore()
+    })
+
+    it('should call fetch fucntion', () => {
+      const artists = search()
+      expect(stubedFetch).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      context('passing one type', () => {
+        const artists = search('Incubus', 'artist')
+        expect(stubedFetch).to.have.been.calledWith(
+          'https://api.spotify.com/v1/search?q=Incubus&type=artist'
+        )
+
+        const albums = search('Incubus', 'album')
+        expect(stubedFetch).to.have.been.calledWith(
+          'https://api.spotify.com/v1/search?q=Incubus&type=album'
+        )
+      })
+
+      context('passing more then one type', () => {
+        const artistsAndAlbums = search('Incubus', ['artist', 'album'])
+        expect(stubedFetch).to.have.been.calledWith(
+          'https://api.spotify.com/v1/search?q=Incubus&type=artist,album'
+        )
+      })
+    })
+
+    it('should return the JSON Data from Promise', () => {
+      promise.resolves({ body: 'json' })
+      const artists = search('Incubus', 'artist')
+
+      expect(artists.resolveValue).to.be.eql({ body: 'json' })
+      // artists.then(data => expect(data).to.be.eql({ body: 'json' }))
+    })
+  })
+})
