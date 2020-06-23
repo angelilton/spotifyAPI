400b5b6f2898aef5142d130caf53fb4ff6faa039
mac <mac@macs-MacBook-Pro.local>
2020-06-24 00:47:41 +0100

    tests for all methods

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,7 @@
 import chai, { expect } from 'chai'
-import Sinon from 'sinon'
+import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
+import sinonStubPromise from 'sinon-stub-promise'
 
 import {
   search,
@@ -10,10 +11,24 @@ import {
   searchPlaylists
 } from '../src/main'
 
-global.fetch = require('node-fetch')
 chai.use(sinonChai)
+sinonStubPromise(sinon)
+
+global.fetch = require('node-fetch')
 
 describe('Spotify API', () => {
+  let stubedFetch
+  let promise
+
+  beforeEach(() => {
+    stubedFetch = sinon.stub(global, 'fetch')
+    promise = stubedFetch.returnsPromise()
+  })
+
+  afterEach(() => {
+    stubedFetch.restore()
+  })
+
   describe('smoke test', () => {
     it('should exist the search method', () => {
       expect(search).to.exist
@@ -37,17 +52,6 @@ describe('Spotify API', () => {
   })
 
   describe('Generic Search', () => {
-    let stubedFetch
-    let promise
-
-    beforeEach(() => {
-      stubedFetch = Sinon.stub(global, 'fetch')
-      promise = stubedFetch.resolves({ json: () => ({ body: 'json' }) })
-    })
-    afterEach(() => {
-      stubedFetch.restore()
-    })
-
     it('should call fetch fucntion', () => {
       const artists = search()
       expect(stubedFetch).to.have.been.calledOnce
@@ -79,7 +83,81 @@ describe('Spotify API', () => {
       const artists = search('Incubus', 'artist')
 
       expect(artists.resolveValue).to.be.eql({ body: 'json' })
-      // artists.then(data => expect(data).to.be.eql({ body: 'json' }))
+    })
+  })
+
+  describe('searchAtists', () => {
+    it('should call fetch function', () => {
+      const artists = searchArtists('Incubus')
+      expect(stubedFetch).to.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      const artists = searchArtists('Incubus')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Incubus&type=artist'
+      )
+
+      const artists2 = searchArtists('Muse')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Incubus&type=artist'
+      )
+    })
+  })
+  describe('searchAlbums', () => {
+    it('should call fetch function', () => {
+      const albums = searchAlbums('Incubus')
+      expect(stubedFetch).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      const albums = searchAlbums('Incubus')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Incubus&type=album'
+      )
+
+      const albums2 = searchAlbums('Muse')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Muse&type=album'
+      )
+    })
+  })
+
+  describe('searchTracks', () => {
+    it('should call fetch function', () => {
+      const tracks = searchTracks('Incubus')
+      expect(stubedFetch).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      const tracks = searchTracks('Incubus')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Incubus&type=track'
+      )
+
+      const tracks2 = searchTracks('Muse')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Muse&type=track'
+      )
+    })
+  })
+
+  describe('searchPlaylists', () => {
+    it('should call fetch function', () => {
+      const playlists = searchPlaylists('Incubus')
+      expect(stubedFetch).to.have.been.calledOnce
+    })
+
+    it('should call fetch with the correct URL', () => {
+      const playlists = searchPlaylists('Incubus')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Incubus&type=playlist'
+      )
+
+      const playlists2 = searchPlaylists('Muse')
+      expect(stubedFetch).to.have.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Muse&type=playlist'
+      )
     })
   })
 })
