6bee42ec43f9ef9d023c141f2bbe19a6f8a16235
mac <mac@macs-MacBook-Pro.local>
2020-07-20 17:21:14 +0100

    Edit to webpack UMD

--- examples/albums.js
+++ examples/albums.js
@@ -1,5 +1,9 @@
-import { searchAlbums } from '../src/index'
+import SpotifyAPI from '../src/index'
 global.fetch = require('node-fetch')
 
-const albums = searchAlbums('muse')
+const spotify = new SpotifyAPI({
+  token:
+    'BQAypnkKhcbkG2kb9pHWX3WLkws_QBJR0AwpVCHGec37oXYswpdm0CDxCbDPoPwSHsV40dnutr1_DH-IffsrF9CiRSeaCg2BHtz8KJ8-7Sr6Gmb7XSedgbc_01HMkIHaIiQXnDok555863dYObPKMjESl_c4ZIJozDg6Vna6bA_tTjzh6KdogNbAJZYUFUUnwjH27Q'
+})
+const albums = spotify.search.albums('tiesto')
 albums.then(data => data.albums.items.map(item => console.log(item.name)))
